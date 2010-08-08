40a147bdb2ea30413aee28180d86f0583e262d79
lukasz.kulig88 <lukasz.kulig88@d273b20c-fbe8-7104-7119-922bfd91e8c4>
2010-08-08 11:05:37 +0000

    GUI - pierwsza, tetsowa implementacja. Do weryfikacji.

---
+++ StudiesPlans/StudiesPlans/MainForm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace StudiesPlans
+{
+    public partial class MainForm : Form
+    {
+        Boolean isManagementShown = false;
+        private
+             SubjectManagement subjectManagementForm = new SubjectManagement();
+
+        public MainForm()
+        {
+           
+            InitializeComponent();
+        }
+
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FormCollection fc = Application.OpenForms;
+            foreach (Form f in fc)
+            {
+                if (f is SlideForm)
+                {
+                    isManagementShown = true;
+                    f.Focus();
+                    break;
+                }
+            }
+
+            if (!isManagementShown)
+            {
+                subjectManagementForm = new SubjectManagement();
+                subjectManagementForm.Show();
+                isManagementShown = true;
+            }
+            else
+            {
+                isManagementShown = false;
+                subjectManagementForm.Focus();
+                subjectManagementForm.Dispose();
+            }
+        }
+    }
+}
