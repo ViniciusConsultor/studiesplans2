3702d5d4cad5574d103a6b15b01ede02dc08be66
salat25@gmail.com <salat25@gmail.com@d273b20c-fbe8-7104-7119-922bfd91e8c4>
2010-10-18 12:58:15 +0000

    1. Przerzuciłem repozytoria do osobnego folderu bo się zamiesznaie robiło; 2. Parę nowych klas do dodawania przedmiotu do bazy;

---
+++ trunk/StudiesPlans/StudiesPlansModels/Models/SubjectDataModels.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudiesPlansModels.Models
+{
+    public class NewSubject : BaseModel
+    {
+        [Required(ErrorMessage = "Nazwa przedmiotu jest wymagana")]
+        [StringLength(150, ErrorMessage = "Nazwa może mieć maskymalnie 150 znaków")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Wydział jest wymagany")]
+        public int DepartamentId { get; set; }
+
+        [Required(ErrorMessage = "Kierunek jest wymagany")]
+        public int FacultyId { get; set; }
+
+        [Required(ErrorMessage = "Instytut jest wymagany")]
+        public int InstituteId { get; set; }
+
+        public bool IsExam { get; set; }
+
+        [Required(ErrorMessage = "Semestr jest wymagany")]
+        public int SemesterId { get; set; }
+
+        [Required(ErrorMessage = "Punkty ECTS są wymagane")]
+        public double Ects { get; set; }
+
+        public IEnumerable<NewSubjectTypeData> SubjectTypes { get; set; }
+    }
+}
