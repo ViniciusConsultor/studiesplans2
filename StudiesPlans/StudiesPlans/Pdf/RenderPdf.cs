eef1454a769fd505dcad3c561ee340612a63dcaf
salat25 <salat25@gmail.com>
2010-12-09 20:47:29 +0000

    1. Poprawki w wyświetlaniu podglądu - tabela się nie rozjeżdża; 2. Specjalizacje w podglądzie; 3. Legenda w podglądzie

--- StudiesPlans/StudiesPlans/Pdf/RenderPdf.cs
+++ StudiesPlans/StudiesPlans/Pdf/RenderPdf.cs
@@ -61,7 +61,7 @@ namespace StudiesPlans.Pdf
                 foreach (SubjectsData sd in LoadedPlan.SubjectsDatas)
                 {
                     bool semOnList = false;
-                    bool stOnList = false;
+                    
                     foreach (Semester s in semesters)
                     {
                         if (s.SemesterID == sd.SemesterID)
@@ -70,6 +70,7 @@ namespace StudiesPlans.Pdf
 
                     foreach (SubjectTypesData st in sd.SubjectTypesDatas)
                     {
+                        bool stOnList = false;
                         foreach (SubjectTypesData std in subjectTypes)
                             if (st.SubjectTypeID == std.SubjectTypeID)
                                 stOnList = true;
@@ -105,11 +106,12 @@ namespace StudiesPlans.Pdf
                         Size tmp = System.Windows.Forms.TextRenderer.MeasureText(sd.Subject.Name, ff);
                         subjectNames.Add(sd.Subject.Name);
                         if (tmp.Width > nameLength.Width)
-                            nameLength = tmp;
+                            nameLength.Width = tmp.Width;
                     }
                 }
-                
-                tablePositionX = nameLength.Width + 2;
+
+                // -32
+                tablePositionX += nameLength.Width + 34;
 
                 tablePositionY -= 11;
                 gfx.DrawLine(pen, tablePositionX, tablePositionY, tablePositionX, tablePositionY2);
@@ -119,14 +121,14 @@ namespace StudiesPlans.Pdf
                 if (semLength + 40 < semNameLength.Width)
                     diff = semNameLength.Width - semLength - 40;
                 else if (semLength >= semNameLength.Width)
-                    diff = 0;
+                    diff = 40;
                 else
                     diff = 40;
 
                 semLength += diff;
 
                 foreach (Semester s in semesters)
-                {  
+                {
                     gfx.DrawString(s.Name, font, XBrushes.Black, tablePositionX + 2, tablePositionY + 11);
                     foreach (SubjectTypesData st in subjectTypes)
                     {
@@ -151,30 +153,54 @@ namespace StudiesPlans.Pdf
                 double oldY2 = tablePositionY2 + 15;
                 int lp = 1;
 
-                fullLength = nameLength.Width + (semLength * semesters.Count) + 2;
+                fullLength = nameLength.Width + (semLength * semesters.Count) + 49;
                 gfx.DrawLine(pen, tablePositionX, 30, fullLength, 30);
                 gfx.DrawLine(pen, tablePositionX, tablePositionY2, fullLength, tablePositionY2);
-                gfx.DrawLine(pen, nameLength.Width + 2, tablePositionY2 - 15, fullLength, tablePositionY2 - 15);
+                gfx.DrawLine(pen, nameLength.Width + 49, tablePositionY2 - 15, fullLength, tablePositionY2 - 15);
+                nameLength.Width += 47;
+
+                int nonSpecCount = 0;
+
                 foreach (string name in subjectNames)
                 {
-                    int offset = 0;
-                    tablePositionY = tablePositionY2;
-                    tablePositionY2 += 15;
-                    gfx.DrawLine(pen, tablePositionX, tablePositionY, tablePositionX, tablePositionY2);
-                    offset += 2;
-                    gfx.DrawString(lp.ToString(), font, XBrushes.Black, tablePositionX + offset, tablePositionY + 11);
-                    gfx.DrawLine(pen, tablePositionX, tablePositionY2, fullLength, tablePositionY2);
-                    offset += 28;
-                    gfx.DrawLine(pen, tablePositionX + 30, tablePositionY, tablePositionX + offset, tablePositionY2);
-                    lp++;
-                    offset += 2;
-                    gfx.DrawString(name, font, XBrushes.Black, tablePositionX + offset, tablePositionY + 11);
-                    gfx.DrawLine(pen, nameLength.Width + 2, tablePositionY, nameLength.Width + 2, tablePositionY2);
+                    //int offset = 0;
+                    //tablePositionY = tablePositionY2;
+                    //tablePositionY2 += 15;
+                    //gfx.DrawLine(pen, tablePositionX, tablePositionY, tablePositionX, tablePositionY2);
+                    //offset += 2;
+                    //gfx.DrawString(lp.ToString(), font, XBrushes.Black, tablePositionX + offset, tablePositionY + 11);
+                    //gfx.DrawLine(pen, tablePositionX, tablePositionY2, fullLength, tablePositionY2);
+                    //offset += 28;
+                    //gfx.DrawLine(pen, tablePositionX + 30, tablePositionY, tablePositionX + offset, tablePositionY2);
+                    //lp++;
+                    //offset += 2;
+                    //gfx.DrawString(name, font, XBrushes.Black, tablePositionX + offset, tablePositionY + 11);
+                    //gfx.DrawLine(pen, nameLength.Width + 2, tablePositionY, nameLength.Width + 2, tablePositionY2);
 
                     foreach (SubjectsData sd in LoadedPlan.SubjectsDatas)
                     {
-                        if (sd.Subject.Name.Equals(name))
+                        if (sd.Subject.Name.Equals(name) && sd.SpecializationsData == null)
                         {
+                            nonSpecCount++;
+                            int offset = 0;
+                            tablePositionY = tablePositionY2;
+                            tablePositionY2 += 15;
+                            gfx.DrawLine(pen, tablePositionX, tablePositionY, tablePositionX, tablePositionY2);
+                            offset += 2;
+                            gfx.DrawString(lp.ToString(), font, XBrushes.Black, tablePositionX + offset, tablePositionY + 11);
+                            gfx.DrawLine(pen, tablePositionX, tablePositionY2, fullLength, tablePositionY2);
+                            offset += 28;
+                            gfx.DrawLine(pen, tablePositionX + 30, tablePositionY, tablePositionX + offset, tablePositionY2);
+                            lp++;
+                            offset += 2;
+
+                            if (sd.IsElective)
+                                gfx.DrawString(name, font, XBrushes.Red, tablePositionX + offset, tablePositionY + 11);
+                            else
+                                gfx.DrawString(name, font, XBrushes.Black, tablePositionX + offset, tablePositionY + 11);
+
+                            gfx.DrawLine(pen, nameLength.Width + 2, tablePositionY, nameLength.Width + 2, tablePositionY2);
+
                             int no = 0;
                             foreach (Semester s in semesters)
                             {
@@ -204,7 +230,7 @@ namespace StudiesPlans.Pdf
 
                 // draw grid
                 tablePositionX = oldX + 32 + nameLength.Width;
-                for (int i = 0; i < subjectNames.Count; i++)
+                for (int i = 0; i < nonSpecCount; i++)
                 {
                     for (int j = 0; j < semesters.Count; j++)
                     {
@@ -220,7 +246,117 @@ namespace StudiesPlans.Pdf
                     oldY = oldY2;
                     oldY2 += 15;
                 }
+
+                List<string> specializations = new List<string>();
+                foreach (SubjectsData sd in LoadedPlan.SubjectsDatas)
+                {
+                    if (sd.SpecializationsData != null)
+                    {
+                        bool wasOnList = false;
+                        foreach (string spec in specializations)
+                            if (spec.Equals(sd.SpecializationsData.Specialization.Name))
+                                wasOnList = true;
+
+                        if (!wasOnList)
+                            specializations.Add(sd.SpecializationsData.Specialization.Name);
+                    }
+                }
+
+                oldY2 += 30;
+                foreach (string specialization in specializations)
+                {
+                    lp = 1;
+                    tablePositionY += 45;
+                    tablePositionY2 = tablePositionY;
+                    oldY = tablePositionY;
+                    oldY2 = tablePositionY2;
+                    tablePositionX = 15;
+                    gfx.DrawString(specialization, font, XBrushes.Black, 15, tablePositionY-5);
+                    gfx.DrawLine(pen, tablePositionX, tablePositionY, fullLength, tablePositionY);
+                    int specCount = 1;
+                    foreach (string name in subjectNames)
+                    {
+                        foreach (SubjectsData sd in LoadedPlan.SubjectsDatas)
+                        {
+                            if (sd.Subject.Name.Equals(name) && sd.SpecializationsData != null && sd.SpecializationsData.Specialization.Name.Equals(specialization))
+                            {
+                                specCount++;
+                                int offset = 0;
+                                tablePositionY = tablePositionY2;
+                                tablePositionY2 += 15;
+                                gfx.DrawLine(pen, tablePositionX, tablePositionY, tablePositionX, tablePositionY2);
+                                offset += 2;
+                                gfx.DrawString(lp.ToString(), font, XBrushes.Black, tablePositionX + offset, tablePositionY + 11);
+                                gfx.DrawLine(pen, tablePositionX, tablePositionY2, fullLength, tablePositionY2);
+                                offset += 28;
+                                gfx.DrawLine(pen, tablePositionX + 30, tablePositionY, tablePositionX + offset, tablePositionY2);
+                                lp++;
+                                offset += 2;
+
+                                if (sd.SpecializationsData.IsElective)
+                                    gfx.DrawString(name, font, XBrushes.Red, tablePositionX + offset, tablePositionY + 11);
+                                else
+                                    gfx.DrawString(name, font, XBrushes.Black, tablePositionX + offset, tablePositionY + 11);
+
+                                gfx.DrawLine(pen, nameLength.Width + 2, tablePositionY, nameLength.Width + 2, tablePositionY2);
+
+                                int no = 0;
+                                foreach (Semester s in semesters)
+                                {
+                                    if (s.Name.Equals(sd.Semester.Name))
+                                    {
+                                        foreach (SubjectTypesData std in sd.SubjectTypesDatas)
+                                        {
+                                            int stno = 0;
+                                            foreach (SubjectTypesData std2 in subjectTypes)
+                                            {
+                                                if (std.SubjectType.Name.Equals(std2.SubjectType.Name))
+                                                    gfx.DrawString(std.Hours.ToString(), font, XBrushes.Black, (nameLength.Width + 4) + (no * semLength) + (15 * stno), tablePositionY + 11);
+
+                                                stno++;
+                                            }
+                                        }
+                                        if (sd.IsExam)                                                                                  // -53
+                                            gfx.DrawString("x", font, XBrushes.Black, (nameLength.Width + 4) + ((no + 1) * (semLength) - 13 - diff), tablePositionY + 11);
+                                        // -38              
+                                        gfx.DrawString(sd.Ects.ToString(), font, XBrushes.Black, (nameLength.Width + 4) + ((no + 1) * (semLength) + 2 - diff), tablePositionY + 11);
+                                    }
+                                    no++;
+                                }
+                            }
+                        }
+                    }
+
+                    tablePositionX = oldX + 32 + nameLength.Width;
+                    for (int i = 0; i < specCount; i++)
+                    {
+                        for (int j = 0; j < semesters.Count; j++)
+                        {
+                            for (int k = 0; k < subjectTypes.Count; k++)
+                                gfx.DrawLine(pen, (nameLength.Width + 15) + 2 + (j * semLength) + (15 * k), oldY, (nameLength.Width + 15) + 2 + (j * semLength) + (15 * k), oldY2);
+                            gfx.DrawLine(pen, (nameLength.Width + 2) + ((j + 1) * (semLength) - diff), oldY, (nameLength.Width + 2) + ((j + 1) * (semLength) - diff), oldY2);
+                            gfx.DrawLine(pen, (nameLength.Width + 2) + ((j + 1) * semLength), oldY, (nameLength.Width + 2) + ((j + 1) * semLength), oldY2);
+
+                        }
+                        oldY = oldY2;
+                        oldY2 += 15;
+                    }
+                }
+
+                // draw legend
+
+                tablePositionX = 15;
+                tablePositionY += 50;
+                foreach (SubjectTypesData std in subjectTypes)
+                {
+                    gfx.DrawString(std.SubjectType.Name.Substring(0, 1) + " - " + std.SubjectType.Name, font, XBrushes.Black, tablePositionX, tablePositionY);
+                    tablePositionY += 15; 
+                }
+                gfx.DrawString("E - Egzamin", font, XBrushes.Black, tablePositionX, tablePositionY);
+                gfx.DrawString("Kolorem czerwonym oznaczono przedmioty obieralne, kolorem czarnym - przedmioty obowiązkowe", font, XBrushes.Black, tablePositionX, tablePositionY+15);
             }
+
+
             if (titleSize.Width > fullLength)
                 this.Width = titleSize.Width + 30;
             else
